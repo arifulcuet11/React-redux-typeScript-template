c30d563500cdd937f52db180d1c58e89984cf3db
ariful islam <ariful.islam@shohoz.com>
2022-03-21 16:10:45 +0600

    header add

--- src/components/post.tsx
+++ src/components/post.tsx
@@ -19,28 +19,34 @@ const PostsList = () => {
       <section className="posts-list p-4">
         {postStatus}
         <table className="table-fixed bg-yellow-200">
-          <tr className="bg-gray-50 dark:bg-gray-700">
-            <td>ID</td>
-            <td>Title</td>
-            <td>Body</td>
-            <td>User ID</td>
-          </tr>
-          {posts.map((post) => (
-            <tr key={post.id} className="border-b odd:bg-white even:bg-gray-50 odd:dark:bg-gray-800 even:dark:bg-gray-700 dark:border-gray-600">
-              <td className="p-4">
-                {post.id}
-              </td>
-              <td className="p-4">
-                {post.title}
-              </td>
-              <td className="py-4 p-4">
-                {post.body}
-              </td>
-              <td className="p-4">
-                {post.userId}
-              </td>
+          <thead>
+            <tr className="bg-gray-50 dark:bg-gray-700">
+              <td>ID</td>
+              <td>Title</td>
+              <td>Body</td>
+              <td>User ID</td>
             </tr>
+          </thead>
+
+          <tbody>
+            {posts.map((post) => (
+              <tr key={post.id} className="border-b odd:bg-white even:bg-gray-50 odd:dark:bg-gray-800 even:dark:bg-gray-700 dark:border-gray-600">
+                <td className="p-4">
+                  {post.id}
+                </td>
+                <td className="p-4">
+                  {post.title}
+                </td>
+                <td className="py-4 p-4">
+                  {post.body}
+                </td>
+                <td className="p-4">
+                  {post.userId}
+                </td>
+              </tr>
                 ))}
+          </tbody>
+
         </table>
 
       </section>
